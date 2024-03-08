a5b1af2a963237647df12fd168b231e72bb026e2
Andreas Raftevold Espelund <andreres@ulrik.uio.no>
2024-03-08 17:19:33 +0100

    New stats with 1000000 entries

--- utils.py
+++ utils.py
@@ -26,3 +26,27 @@ def mean_position_error(y_true: pd.DataFrame, y_pred: pd.DataFrame) -> float:
 
     errors = np.array([haversine(true[0], true[1], pred[0], pred[1]) for true, pred in zip(y_true, y_pred)])
     return errors.mean()
+
+
+def load_dataframe(path: str) -> pd.DataFrame:
+    df = pd.read_csv(path)
+
+    return df.astype({
+        "latitude" : "float64",
+        "longitude" : "float64",
+        "PCI_cov" : "int16",
+        "SSBIdx_cov" : "int8",
+        "SSB_RSSI" : "float32",
+        "SSS_SINR" : "float32",
+        "SSS_RSRP" : "float32",
+        "SSS_RSRQ" : "float32",
+        "PPS" : "float64",
+        "ToA_cov" : "float64",
+        "operator_ID_cov" : "int8",
+        "campaign_ID_cov" : "int8",
+        "PCI_cir" : "int16",
+        "SSBIdx_cir" : "int8",
+        "ToA_cir" : "float32",
+        "operator_ID_cir" : "int8",
+        "campaign_ID_cir" : "int8",
+    })
