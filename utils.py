38b9ad95d92f8640bb71f482c11cea4f14f0dc2f
Andreas Raftevold Espelund <andreres@ulrik.uio.no>
2024-03-08 12:35:41 +0100

    Added random forest

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 # geo_utils.py
 import numpy as np
+import pandas as pd
 
-def haversine(lat1, lon1, lat2, lon2):
+
+def haversine(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
     """
     Calculate the great circle distance in meters between two points
     on the earth (specified in decimal degrees).
@@ -12,11 +14,15 @@ def haversine(lat1, lon1, lat2, lon2):
     # Haversine formula
     dlat = lat2 - lat1
     dlon = lon2 - lon1
-    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    a = np.sin(dlat / 2) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2) ** 2
     c = 2 * np.arcsin(np.sqrt(a))
     r = 6371000  # Radius of Earth in meters
     return c * r
 
-def mean_position_error(y_true, y_pred):
+
+def mean_position_error(y_true: pd.DataFrame, y_pred: pd.DataFrame) -> float:
+    y_true = np.array(y_true, dtype=float)
+    y_pred = np.array(y_pred, dtype=float)
+
     errors = np.array([haversine(true[0], true[1], pred[0], pred[1]) for true, pred in zip(y_true, y_pred)])
     return errors.mean()
